a189db52f67b94bdeb30d18077cf53366f036d58
Daniel Perez <daniel@claudetech.com>
2015-11-30 18:49:33 +0900

    Allow to limit the number of images to choose.

--- src/directives/image-picker.js
+++ src/directives/image-picker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var imagePickerFactory = function ($interpolate) {
+var imagePickerFactory = function ($parse) {
   return {
     template: require('../templates/image-picker.jade')(),
     restrict: 'EA',
@@ -12,10 +12,18 @@ var imagePickerFactory = function ($interpolate) {
       '$attrs',
       'ionimgMediaLister',
       function ($scope, $attrs, ionimgMediaLister) {
+        var maxImagesCount;
+        var replaceOnMaxReached = false;
+        if ($attrs.maxImagesCount) {
+          maxImagesCount = $parse($attrs.maxImagesCount)($scope.$parent);
+        }
+        if ($attrs.replaceOnMaxReached) {
+          replaceOnMaxReached = $parse($attrs.replaceOnMaxReached)($scope.$parent);
+        }
         var loadOptions = {
           thumbnail: {
-            width: ($attrs.width && $interpolate($attrs.imgWidth)($scope.$parent)) || 128,
-            height: ($attrs.height && $interpolate($attrs.imgHeight)($scope.$parent)) || 128
+            width: ($attrs.width && $parse($attrs.imgWidth)($scope.$parent)) || 128,
+            height: ($attrs.height && $parse($attrs.imgHeight)($scope.$parent)) || 128
           },
           mediaTypes: ['image']
         };
@@ -31,17 +39,40 @@ var imagePickerFactory = function ($interpolate) {
           $scope.selection[image.id] = image;
         });
 
-        $scope.toggleSelection = function (image) {
+        var removeFirstImage = function () {
+          var removed = $scope.selectedImages.splice($scope.selectedImages.length - 1);
+          if (removed[0] && removed[0].id) {
+            delete $scope.selection[removed[0].id];
+          }
+        };
+
+        var updateSelection = function (image) {
           if ($scope.selection[image.id]) {
+            $scope.$emit('ionimg:unselected', image);
             delete $scope.selection[image.id];
           } else {
-            $scope.selection[image.id] = image;
+            if ($scope.selectedImages.length >= maxImagesCount && replaceOnMaxReached) {
+              removeFirstImage();
+            }
+            if ($scope.selectedImages.length < maxImagesCount) {
+              $scope.selection[image.id] = image;
+              $scope.$emit('ionimg:selected', image);
+            } else {
+              $scope.$emit('ionimg:cannot-select', image);
+            }
           }
+        };
+
+        $scope.toggleSelection = function (image) {
+          updateSelection(image);
           $scope.selectedImages.splice(0, $scope.selectedImages.length);
           for (var id in $scope.selection) {
             if ($scope.selection.hasOwnProperty(id)) {
               $scope.selectedImages.push($scope.selection[id]);
             }
+          }
+          if ($scope.selectedImages.length >= maxImagesCount) {
+            $scope.$emit('ionimg:max-reached');
           }
         };
 
